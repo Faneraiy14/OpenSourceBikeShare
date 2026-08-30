7ad183df7173e37e9d87c5ae700c00b29a9b36ca
Faneraiy14 <super.sviatoslav3@gmail.com>
2026-08-30 15:50:01 +0300

    Restore declare(strict_types=1) lost during rebase conflict resolution

--- src/Credit/CodeGenerator/CodeGeneratorInterface.php
+++ src/Credit/CodeGenerator/CodeGeneratorInterface.php
@@ -1,13 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace BikeShare\Credit\CodeGenerator;
 
 interface CodeGeneratorInterface
 {
-
     /**
      * @return array<int, string>
      */
-
     public function generate(int $count, int $length): array;
 }
